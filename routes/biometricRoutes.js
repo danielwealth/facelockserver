23436e619a7320cd84b124b1d7ffbe7b0713a52c
ohidans <74150381+danielwealth@users.noreply.github.com>
2025-11-07 14:00:42 +0100

    Add express import to biometricRoutes.js

--- routes/biometricRoutes.js
+++ routes/biometricRoutes.js
@@ -1,4 +1,4 @@
-
+const express = require('express');
 const router = express.Router();
 router.post('/verify-authentication', async (req, res) => {
   const verification = await verifyAuthenticationResponse({
