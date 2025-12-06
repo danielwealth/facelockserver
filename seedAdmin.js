1853be85bff4010032618df29427911b6f735d9e
ohidans <74150381+danielwealth@users.noreply.github.com>
2025-12-06 01:48:51 +0100

    Use environment variable for MongoDB connection URI

--- seedAdmin.js
+++ seedAdmin.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const User = require('./models/User');
 
-mongoose.connect('mongodb://localhost:27017/yourdb');
+mongoose.connect(process.env.MONGO_URI);
 
 async function createAdmin() {
   const adminExists = await User.findOne({ role: 'admin' });
