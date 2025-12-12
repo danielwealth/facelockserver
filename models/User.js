f8dfb185740959142490c9a8b94513609cf81ef8
ohidans <74150381+danielwealth@users.noreply.github.com>
2025-12-12 03:15:49 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -31,7 +31,7 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   try {
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(12);
     this.password = await bcrypt.hash(this.password, salt);
     next();
   } catch (err) {
