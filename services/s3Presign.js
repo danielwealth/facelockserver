0a37f4cd2c6f40d635996e30514fa0108f5c6de5
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-05-08 05:52:35 +0100

    Refactor S3 presign module and add exports

--- services/s3Presign.js
+++ services/s3Presign.js
@@ -1,17 +1,27 @@
-// server/services/s3Presign.js
+// services/s3Presign.js
 const AWS = require('aws-sdk');
+
+// Configure S3 client
 const s3 = new AWS.S3({ region: process.env.AWS_REGION });
 
+/**
+ * Create a presigned URL for uploading a file to S3
+ * @param {Object} params - { key, contentType, expiresSeconds }
+ * @returns {Promise<Object>} { url, key }
+ */
 async function createPresignedPutKey({ key, contentType, expiresSeconds = 300 }) {
   const params = {
     Bucket: process.env.AWS_S3_BUCKET,
     Key: key,
     ContentType: contentType,
     ACL: 'private',
-    Expires: expiresSeconds
+    Expires: expiresSeconds,
   };
+
   const url = await s3.getSignedUrlPromise('putObject', params);
   return { url, key };
 }
 
-module.exports = { createPresignedPutKey };
+module.exports = {
+  createPresignedPutKey,
+};
