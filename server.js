2252aee35b746a27416370cc918f4a3e0c87d4d4
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-05-09 13:19:09 +0100

    Update CORS configuration to use allowed origins
    Refactor CORS middleware to allow specific origins from environment variable.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const path = require('path');
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || '').split(',');
 
 // Route modules
 const authRoutes = require('./routes/auth');
@@ -30,8 +31,15 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN || 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
